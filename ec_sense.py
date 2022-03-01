0e99b45a658410c6cc119e695dcd8824f50ca59e
= <=>
2022-03-01 12:57:47 +0100

    added sensor control file

---
+++ ec_sense.py
@@ -0,0 +1,9 @@
+# add here all functions to connect with the ec noc sensor
+
+# init routines
+# get_temperature
+# get_humidity
+# get_gas_concentration
+#
+def con_test():
+    return "Connection to ec_sense file works"
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import ec_sense
 from guizero import App, Text
 
-app = App(title="test ", width=300, height=400)
-print("app is running")
 
-app.display()
+# touch display_size = 800x480 px
+app = App(title="AirQ v1.0 ", layout="grid", visible=True, bg="white")
+
+text = Text(app, text="hello world")
+
+
+# run as main application
+if __name__ == "__main__":
+    app.display()
